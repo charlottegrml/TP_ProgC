e0735984a3a4df4475191db6fa2a6bb2dcd866b7
charlottegrml <charlotte.gourmelond@gmail.com>
2026-01-29 11:45:59 +0100

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,58 @@
+#include <stdio.h>
+
+int main() {
+
+    int num1 = 12;
+    int num2 = 5;
+    char op = '+';
+
+    switch (op) {
+
+        case '+':
+            printf("Addition : %d\n", num1 + num2);
+            break;
+
+        case '-':
+            printf("Soustraction : %d\n", num1 - num2);
+            break;
+
+        case '*':
+            printf("Multiplication : %d\n", num1 * num2);
+            break;
+
+        case '/':
+            if (num2 != 0) {
+                printf("Division : %d\n", num1 / num2);
+            } else {
+                printf("Erreur : division par zero\n");
+            }
+            break;
+
+        case '%':
+            if (num2 != 0) {
+                printf("Modulo : %d\n", num1 % num2);
+            } else {
+                printf("Erreur : modulo par zero\n");
+            }
+            break;
+
+        case '&':
+            printf("ET binaire : %d\n", num1 & num2);
+            break;
+
+        case '|':
+            printf("OU binaire : %d\n", num1 | num2);
+            break;
+
+        case '~':
+            printf("NON binaire sur num1 : %d\n", ~num1);
+            break;
+
+        default:
+            printf("Operateur non reconnu\n");
+    }
+
+    return 0;
+}
+
 
